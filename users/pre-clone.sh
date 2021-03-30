229eb9c595d32e1d1d26244343ea05521d023046
Viet Tran <tdviet@gmail.com>
2021-03-31 00:27:20 +0200

    A working script for full automation
    Based on Mario's script, with automatic setting OS_STORAGE_URL and some new features (oidc-agent, new output format for  "fedcloud site show-project-id")

---
+++ users/pre-clone.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# If using oidc-agent for providing access token, set the account name here
+# otherwise comment out following two lines
+
+export OIDC_AGENT_ACCOUNT=egi
+eval `oidc-keychain --accounts ${OIDC_AGENT_ACCOUNT}`
+
+# If providing access token directly,e.g. from  https://aai.egi.eu/fedcloud/
+# uncomment the following line and set access token there
+
+#export OIDC_ACCESS_TOKEN=<add-access-token-here>
+
+# Set site and VO
+
+export EGI_SITE=NCG-INGRID-PT
+export EGI_VO=covid19.eosc-synergy.eu
+
+
+# Beginning of the script
+# Do not change following lines
+
+unset OS_ACCESS_TOKEN OS_AUTH_TYPE OS_AUTH_URL OS_IDENTITY_PROVIDER OS_PROJECT_ID OS_PROTOCOL OS_TOKEN OS_AUTH_TOKEN
+
+export OS_STORAGE_URL=`fedcloud openstack catalog list --json-output |jq -r  '.[].Result[] | select(.Name == "swift") | .Endpoints[] | select(.interface == "public") | .url '`
+
+eval `fedcloud site show-project-id`
+eval "$(fedcloud endpoint env --project-id ${OS_PROJECT_ID})"
+eval "$(fedcloud endpoint token)"
+
+export OS_AUTH_TOKEN=${OS_TOKEN}
+export OS_AUTH_TYPE=v3token
